2a1e319feec8b1bd8e151586da30dbf4514a8f92
hoangnh-vnext <hoangnh@vnext.vn>
2022-12-28 17:27:45 +0700

    Create

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,15 +1,17 @@
+import { Liff } from "@line/liff/dist/lib";
 import Head from "next/head";
 import packageJson from "../../package.json";
 import styles from "../../styles/Auth.module.css"
 
 export default function Auth(props: any) {
-  const liff = props.liff;
+  const liff: Liff = props.liff;
   return (
     <div>
       <Head>
         <title>Home page</title>
       </Head>
       <h1>Homepage</h1>
+      <h1>{liff.getOS()}</h1>
     </div>
   );
 }
