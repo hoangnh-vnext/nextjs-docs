63c0be511c5563216fc704bcc50b3be2e10502df
hoangnh-vnext <hoangnh@vnext.vn>
2022-12-28 17:29:23 +0700

    Create

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -5,13 +5,15 @@ import styles from "../../styles/Auth.module.css"
 
 export default function Auth(props: any) {
   const liff: Liff = props.liff;
+  console.log(liff);
+  
   return (
     <div>
       <Head>
         <title>Home page</title>
       </Head>
       <h1>Homepage</h1>
-      <h1>{liff.getOS()}</h1>
+      {/* <h1>{liff.getOS()}</h1> */}
     </div>
   );
 }
